44ae862553e1dcfc6919feacc4ad7728dc95e932
Asim <asim.rafaq@arbisoft.com>
2024-05-28 16:04:54 +0500

    addTwoNum activity.

---
+++ addTwoNum.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+sum = a + b
+
+print('Sum of {0} and {1} is: {2}'.format(a,b,sum))
+
+#===================================================#
+
+a = 20.5
+b = 30.5
+sum = a + b
+print('sum of {0} and {1} is: {2}'.format(a,b,sum))
